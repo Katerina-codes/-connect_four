85334d79e3a3d5a5cad8a9a93e6b912283a3f50d
Katerina <katerinaldg@gmail.com>
2017-06-29 13:19:37 +0100

    Draws a grid with 7 columns and 6 rows

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,3 +1,5 @@
 class Grid
-
+  def draw_grid
+    "[][][][][][][]\n" * 6
+  end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -1,5 +1,12 @@
 require 'grid'
 
 describe Grid do
+  it "draws a grid of 6 rows by 7 columns" do
+    grid = Grid.new
+    expect(grid.draw_grid).to eq(draw_grid)
+  end
 
+  def draw_grid
+    "[][][][][][][]\n" * 6
+  end
 end
