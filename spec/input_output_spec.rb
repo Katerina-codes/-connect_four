91cf07456d57dc0718540a79c0bde55eb56fa564
Katerina <katerinaldg@gmail.com>
2017-06-30 10:37:12 +0100

    Corrected test name

--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -15,7 +15,7 @@ describe InputOutput do
 """)
 end
 
-  it "gets a move from the player" do
+  it "gets a column number from the player" do
     input = StringIO.new("0")
     grid = InputOutput.new(output, input)
     expect(grid.get_column).to eq("0")
