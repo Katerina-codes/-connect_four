ccaa9fa2f245a593e5095670ed54d2a05daa75ce
Katerina <katerinaldg@gmail.com>
2017-06-30 10:52:45 +0100

    Places a move on a grid

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,4 +2,9 @@ class Grid
   def draw_grid
     Array.new(6) { Array.new(7, "[]") }
   end
+
+  def place_move(grid, row_number, column_number, mark)
+    grid[row_number][column_number] = mark
+    grid
+  end
 end
--- spec/grid_spec.rb
+++ spec/grid_spec.rb
@@ -6,6 +6,11 @@ describe Grid do
     expect(grid.draw_grid).to eq(draw_grid)
   end
 
+  it "places a player move" do
+    grid = Grid.new
+    expect(grid.place_move(draw_grid, 0, 0, "0")).to eq([["0", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"]])
+  end
+
   def draw_grid
     Array.new(6) { Array.new(7, "[]") }
   end
