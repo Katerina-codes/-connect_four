ecc394998ec0da171cc1ab89358bbe8f1df06d59
Katerina <katerinaldg@gmail.com>
2017-07-01 18:27:27 +0100

    Corrected test to given input

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,10 @@ require 'game'
 
 describe Game do
   it "gets and marks a player's move" do
-    game = Game.new
+    input = StringIO.new("0\n0")
+    output = StringIO.new
+    input_output = InputOutput.new(output, input)
+    game = Game.new(input_output)
     expect(game.play_move).to eq([["[0]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"]])
   end
 end
