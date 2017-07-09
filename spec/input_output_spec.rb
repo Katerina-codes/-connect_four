04f0b3591a7c1f273d11ecf69cd9dc4d41e7dc77
Katerina <katerinaldg@gmail.com>
2017-07-09 13:46:04 +0300

    Refactored tests

--- spec/input_output_spec.rb
+++ spec/input_output_spec.rb
@@ -3,9 +3,10 @@ require 'input_output'
 describe InputOutput do
 
   let(:output) { StringIO.new }
+  let(:input) { StringIO.new }
+  let(:input_output) { InputOutput.new(output, input) }
 
   it "displays the grid to the players" do
-    input_output = InputOutput.new(output)
     input_output.display_grid([["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"], ["[]", "[]", "[]", "[]", "[]", "[]", "[]"]])
     expect(output.string).to eq(
 """[][][][][][][]
@@ -24,12 +25,10 @@ end
   end
 
   it "returns false if row input is 0" do
-    input_output = InputOutput.new
     expect(input_output.move_valid_for_row?(0)).to eq(false)
   end
 
     it "displays error message for invalid input" do
-      input_output = InputOutput.new(output)
       input_output.display_error_message
       expect(output.string).to eq("Please enter a valid move\n")
     end
@@ -42,17 +41,14 @@ end
     end
 
     it "returns false if column input is 0" do
-      input_output = InputOutput.new
       expect(input_output.move_valid_for_column?(0)).to eq(false)
     end
 
     it "returns true if column input is 1" do
-      input_output = InputOutput.new
       expect(input_output.move_valid_for_column?(1)).to eq(true)
     end
 
     it "returns true if column input is 8" do
-      input_output = InputOutput.new
       expect(input_output.move_valid_for_column?(8)).to eq(false)
     end
 
