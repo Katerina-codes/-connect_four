af484414526928dba9e1e9aecf037f2dfa20b24b
Katerina <katerinaldg@gmail.com>
2017-07-12 09:19:49 +0300

    Added new behaviour to game_flow

--- lib/game.rb
+++ lib/game.rb
@@ -11,10 +11,19 @@ class Game
   def game_flow
     grid = @grid.draw_grid
     @input_output.display_grid(grid)
+    @input_output.ask_for_row_move("row")
     row_number = @input_output.get_valid_row_move
-    column_number = @input_output.get_move
-    marked_grid = @grid.place_move(grid, row_number, column_number, "0")
-    @input_output.display_grid(marked_grid)
+    @input_output.ask_for_column_move("column")
+    column_number = @input_output.get_valid_column_move
+
+    until !@grid.is_move_unique?(grid, row_number, column_number)
+      @input_output.ask_for_row_move("row")
+      row_number = @input_output.get_valid_row_move
+      @input_output.ask_for_column_move("column")
+      column_number = @input_output.get_valid_column_move
+    end
+  marked_grid = @grid.place_move(grid, row_number, column_number, "0")
+  @input_output.display_grid(marked_grid)
   end
 
 end
