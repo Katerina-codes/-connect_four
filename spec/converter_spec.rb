8c59310ddc5c858b80cd37208085c6a959e0cd08
Katerina <katerinaldg@gmail.com>
2017-07-16 16:56:10 +0300

    Corrected test name

--- spec/converter_spec.rb
+++ spec/converter_spec.rb
@@ -2,9 +2,9 @@ require 'converter'
 
 RSpec.describe Converter do
 
-  it "subtracts 1 from a number" do
+  it "subtracts 1 from a 1" do
     converter = Converter.new
     expect(converter.subtract_one(1)).to eq(0)
   end
-
+  
 end
