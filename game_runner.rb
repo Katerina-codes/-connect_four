a37602fc9d96eb107d965078015e6d66d6c519ab
Katerina <katerinaldg@gmail.com>
2017-07-17 13:45:04 +0100

    Added Converter class

--- game_runner.rb
+++ game_runner.rb
@@ -1,6 +1,7 @@
 require_relative 'lib/game'
 require_relative 'lib/input_output'
 require_relative 'lib/grid'
+require_relative 'lib/converter'
 
-new_game = Game.new(InputOutput.new, Grid.new)
+new_game = Game.new(InputOutput.new, Grid.new, Converter.new)
 new_game.game_flow
