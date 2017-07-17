0bd02ea4a3a54c4fdf41453f87a1f93e6764e393
Katerina <katerinaldg@gmail.com>
2017-07-17 16:34:44 +0100

    corrected test names

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,22 +2,22 @@ require 'game'
 
 RSpec.describe Game do
 
-  it "returns false if moves are [0]" do
+  it "returns [] if moves are [0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won_in_row?([[0]])).to eq([])
   end
 
-  it "returns true if moves are [1, 2, 3, 4]" do
+  it "returns [[1, 2, 3, 4]] if moves are [1, 2, 3, 4]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won_in_row?([[1, 2, 3, 4]])).to eq([[1, 2, 3, 4]])
   end
 
-  it "returns false if moves are [0, 0]" do
+  it "returns [] if moves are [0, 0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won_in_row?([[0, 0]])).to eq([])
   end
 
-  it "returns false if moves are [0, 0, 0]" do
+  it "returns [] if moves are [0, 0, 0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won_in_row?([[0, 0, 0]])).to eq([])
   end
