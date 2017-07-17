c0321d3cf010160fe3dfeee26e4969cdc9e82585
Katerina <katerinaldg@gmail.com>
2017-07-17 16:19:23 +0100

    Corrected test expectation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe Game do
 
   it "returns false if moves are [0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
-    expect(game.is_won?([0])).to eq(false)
+    expect(game.is_won?([[0]])).to eq([])
   end
 
   it "returns true if moves are [1, 2, 3, 4]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
-    expect(game.is_won?([1, 2, 3, 4])).to eq(true)
+    expect(game.is_won?([[1, 2, 3, 4]])).to eq([[1, 2, 3, 4]])
   end
 
   it "returns false if moves are [0, 0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
-    expect(game.is_won?([0, 0])).to eq(false)
+    expect(game.is_won?([[0, 0]])).to eq([])
   end
 
   it "returns false if moves are [0, 0, 0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
-    expect(game.is_won?([0, 0])).to eq(false)
+    expect(game.is_won?([[0, 0, 0]])).to eq([])
   end
 
 end
