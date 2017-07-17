2e724f04f2e56ac4b921e48751074cec5e5dd136
Katerina <katerinaldg@gmail.com>
2017-07-17 15:16:26 +0100

    Made test name better

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,9 +2,9 @@ require 'game'
 
 RSpec.describe Game do
 
-  it "checks if game has been won" do
+  it "returns false if game has not been won" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won?(0)).to eq(false)
   end
-
+  
 end
