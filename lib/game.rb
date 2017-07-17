1a7615db2cf77f68fd0766eddbe22d95186ed004
Katerina <katerinaldg@gmail.com>
2017-07-17 10:39:32 +0100

    Removed unnecessary variable assignment and shorted until loop by using methods

--- lib/game.rb
+++ lib/game.rb
@@ -18,13 +18,13 @@ class Game
   def get_column_move
     @input_output.ask_for_column_move("column")
     column_number = @input_output.get_valid_column_move
-    converted_column_move = @converter.subtract_one(column_number)
+    @converter.subtract_one(column_number)
   end
 
   def get_row_move
     @input_output.ask_for_row_move("row")
     row_number = @input_output.get_valid_row_move
-    converted_row_move = @converter.subtract_one(row_number)
+    @converter.subtract_one(row_number)
   end
 
   def game_flow
@@ -34,10 +34,8 @@ class Game
     converted_column_move = get_column_move
 
     until @grid.is_move_unique?(grid, converted_row_move, converted_column_move)
-      @input_output.ask_for_row_move("row")
-      row_number = @input_output.get_valid_row_move
-      @input_output.ask_for_column_move("column")
-      column_number = @input_output.get_valid_column_move
+      converted_row_move = get_row_move
+      converted_column_move = get_column_move
     end
   marked_grid = @grid.place_move(grid, converted_row_move, converted_column_move, " 0 ")
   @input_output.display_grid(marked_grid)
