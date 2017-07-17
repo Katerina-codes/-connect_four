fed739d5e85cf520d761cfe5f78152dbcbb7e3df
Katerina <katerinaldg@gmail.com>
2017-07-17 15:41:07 +0100

    made test names move specific

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,17 +2,17 @@ require 'game'
 
 RSpec.describe Game do
 
-  it "returns false if game has not been won" do
+  it "returns false if moves are [0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won?([0])).to eq(false)
   end
 
-  it "returns true if game has been won" do
+  it "returns true if moves are [1, 2, 3, 4]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won?([1, 2, 3, 4])).to eq(true)
   end
 
-  it "returns false if game has not been won" do
+  it "returns false if moves are [0, 0]" do
     game = Game.new(InputOutput.new, Grid.new, Converter.new)
     expect(game.is_won?([0, 0])).to eq(false)
   end
